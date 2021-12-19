946c51814996818e70233732dc6f1478308e6b39
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-12-19 17:22:45 +0100

    Better message reception

--- src/server.py
+++ src/server.py
@@ -1,26 +1,28 @@
 import websockets
+import websockets.exceptions
 import asyncio
 
 
 class Server:
     def __init__(self,):
+        self.clients = set()
+
         async def client_handler(websocket, path):
             """
             Manage a connection to the server from a client.
-            :param websocket: URL of the server
-            :param path: URI for the handler
-            :return:
             """
             print("Youhou connecté")
-            await websocket.send("J'suis chauve.")
-
-            while True:
-                data = await websocket.recv()
-                reply = f"Data received as:  {data}!"
-                await websocket.send(reply)
+            self.clients.add(websocket)
+            try:
+                async for message in websocket:  # Getting messages from the client
+                    print("Message from client : \n", message)
+                    # manage_message()
+            except websockets.exceptions.ConnectionClosed as e:
+                print("Client", websocket, " has disconnected")
+                print(e)
+            finally:
+                self.clients.remove(websocket)
 
         start_server = websockets.serve(client_handler, "localhost", 8000)
-
         asyncio.get_event_loop().run_until_complete(start_server)
-
         asyncio.get_event_loop().run_forever()
