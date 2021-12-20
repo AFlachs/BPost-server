abc372246a2cb05f77f619556558b797250470a6
Jeanne 6H <jeanne.szpirer@ulb.be>
2021-12-20 11:06:29 +0100

    Suppression classe Database.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from server import Server
-from Database import *
 
 PORT = 6666
 SEP = "<SEP>"
@@ -7,13 +6,8 @@ SEP = "<SEP>"
 
 def main():
     print("Hello")
-    # serv = Server()
-    # print("Welcome in BPost-Server")
-    test = Database("client_db.db")
-    test.insert_new_client('jszpirer', '1234')
-    test.select_contacts('jszpirer')
-    test.add_contact('jszpirer', 'gszpirer')
-    test.select_contacts('jszpirer')
+    serv = Server()
+    print("Welcome in BPost-Server")
 
 if __name__ == '__main__':
     main()
